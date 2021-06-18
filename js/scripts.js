c604d8c052dbc3d6ef6531042e9c13922394f786
Brandon Eads <brandon181989@hotmail.com>
2021-06-18 14:31:42 -0700

    update html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
+
+
 function Pizza(pizzaSize1, pizzaSize2, pizzaTopping1, pizzaTopping2) {
   this.pizzaSize1 = pizzaSize1;
   this.pizzaSize2 = pizzaSize2;
   this.pizzaTopping1 = pizzaTopping1;
   this.pizzaTopping2 = pizzaTopping2;
 }
+
 
 let finishedPizza = new Pizza("Large", "Extra Large", "Pepperoni", "Mushroom");
 
@@ -40,7 +43,6 @@ Pizza.prototype.completeXL2 = function() {
   return this.pizzaSize2 + " " + this.pizzaTopping1 + " " + this.pizzaTopping2; 
 };
 
-console.log(finishedPizza.pizzaTopping2);
 
 
 let pizzaSize1 = { name: "Large", price: 15.00};
