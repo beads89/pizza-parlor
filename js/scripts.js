7508e6d1a2cd62dbf110a34e6b944eb6fd8fd4f6
Brandon Eads <brandon181989@hotmail.com>
2021-06-27 20:27:41 -0700

    possible refactor js, use checkbox instead of dropdown for top

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
 
 
-function Pizza(pizzaSize1, pizzaSize2, pizzaTopping1, pizzaTopping2) {
-  this.pizzaSize1 = pizzaSize1;
-  this.pizzaSize2 = pizzaSize2;
-  this.pizzaTopping1 = pizzaTopping1;
-  this.pizzaTopping2 = pizzaTopping2;
-  
+function Pizza(pizzaSize, pizzaTopping) {
+  this.pizzaSize = pizzaSize;
+  this.pizzaTopping = pizzaTopping;
+  this.pizzaPrice = 0
 }
 
-let pizzaSize1 = new Pizza('Large');
-let pizzaSize2 = new Pizza('Extra Large');
-let pizzaTopping1 = new Pizza('Pepperoni');
-let pizzaTopping2 = new Pizza('Mushroom');
+Pizza.prototype.size = function(pizzaSize) {
+  if (this.pizzaSize === "pizzaSize1") {
+    this.pizzaPrice += 15
+  } else {
+    this.pizzaPrice += 20
+  }
+}
 
-let completeLarge = new Pizza("Large");
-let completeLargeP = new Pizza("Large", "Pepperoni");
-let completeLargeM = new Pizza("Large", "Mushroom");
-let completeLarge2 = new Pizza("Large", "Pepperoni", "Mushroom");
+Pizza.prototype.topping = function(pizzaTopping) {
+  for (let i = 0; i < this.topping.length; i++) {
+    this.pizzaPrice += 3;
+  }
+};
 
-let completeXL = new Pizza("Extra Large");
-let completeXLP = new Pizza("Extra Large", "Pepperoni");
-let completeXLM = new Pizza("Extra Large", "Mushroom");
-let completeXL2 = new Pizza("Extra Large", "Pepperoni", "Mushroom");
+// let orderedPizza = new Pizza("l")
+
+// let pizzaSize1 = new Pizza('Large');
+// let pizzaSize2 = new Pizza('Extra Large');
+// let pizzaTopping1 = new Pizza('Pepperoni');
+// let pizzaTopping2 = new Pizza('Mushroom');
+
+// let completeLarge = new Pizza("Large");
+// let completeLargeP = new Pizza("Large", "Pepperoni");
+// let completeLargeM = new Pizza("Large", "Mushroom");
+// let completeLarge2 = new Pizza("Large", "Pepperoni", "Mushroom");
+
+// let completeXL = new Pizza("Extra Large");
+// let completeXLP = new Pizza("Extra Large", "Pepperoni");
+// let completeXLM = new Pizza("Extra Large", "Mushroom");
+// let completeXL2 = new Pizza("Extra Large", "Pepperoni", "Mushroom");
 
 
 
