87b31335d055c913f6d44031a2baec3e9aafa2e7
Brandon Eads <brandon181989@hotmail.com>
2021-06-27 23:04:52 -0700

    finish ui, fixing bugs

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Pizza(pizzaSize, pizzaTopping) {
 }
 
 Pizza.prototype.size = function(pizzaSize) {
-  if (this.pizzaSize === "pizzaSize1") {
+  if (this.pizzaSize === "large") {
     this.pizzaPrice += 15
   } else {
     this.pizzaPrice += 20
@@ -15,7 +15,7 @@ Pizza.prototype.size = function(pizzaSize) {
 }
 
 Pizza.prototype.topping = function(pizzaTopping) {
-  for (let i = 0; i < this.topping.length; i++) {
+  for (let i = 0; i <= this.topping.length; i++) {
     this.pizzaPrice += 3;
   }
 };
@@ -23,72 +23,22 @@ Pizza.prototype.topping = function(pizzaTopping) {
 // UI Logic
 
 $(document).ready(function() {
-  $("#pizzaOrder").submit(function(event) {
+  $("form#pizzaOrder").submit(function(event) {
     event.preventDefault();
-    let size = $("#sizes").val();
-    let topping = $("#toppings:checked");
-    let output = Pizza(pizzaSize, pizzaTopping);
-      $("#pizzaComplete").text(output);
+    let sizeInput = $("#sizes").val();
+    let toppingInput = $("#toppings:checked");
+    let toppingOutput = [];
+    toppingInput.each(function() {
+      toppingOutput.push($(this).val());
+    })
+
+    let output = new Pizza(sizeInput, toppingOutput);
+    output.size();
+    output.topping();
+
+    $(".ordered").show();
+    $("#sizeInput").text(output.pizzaSize);
+    $("#pizzaToppings").text(output.pizzaTopping);
+    $("#pizzaCost").text("$" + output.pizzaPrice);
   });
-
-
-// let orderedPizza = new Pizza("l")
-
-// let pizzaSize1 = new Pizza('Large');
-// let pizzaSize2 = new Pizza('Extra Large');
-// let pizzaTopping1 = new Pizza('Pepperoni');
-// let pizzaTopping2 = new Pizza('Mushroom');
-
-// let completeLarge = new Pizza("Large");
-// let completeLargeP = new Pizza("Large", "Pepperoni");
-// let completeLargeM = new Pizza("Large", "Mushroom");
-// let completeLarge2 = new Pizza("Large", "Pepperoni", "Mushroom");
-
-// let completeXL = new Pizza("Extra Large");
-// let completeXLP = new Pizza("Extra Large", "Pepperoni");
-// let completeXLM = new Pizza("Extra Large", "Mushroom");
-// let completeXL2 = new Pizza("Extra Large", "Pepperoni", "Mushroom");
-
-
-
-/*
-Pizza.prototype.completeLarge = function() {
-  return this.pizzaSize1; 
-};
-
-Pizza.prototype.completeLargeP = function() {
-  return this.pizzaSize1 + " " + this.pizzaTopping1; 
-};
-
-Pizza.prototype.completeLargeM = function() {
-  return this.pizzaSize1 + " " + this.pizzaTopping2; 
-};
-
-
-Pizza.prototype.completeLarge2 = function() {
-  return this.pizzaSize1 + " " + this.pizzaTopping1 + " " + this.pizzaTopping2; 
-};
-
-Pizza.prototype.completeXL = function() {
-  return this.pizzaSize2; 
-};
-
-Pizza.prototype.completeXLP = function() {
-  return this.pizzaSize2 + " " + this.pizzaTopping1; 
-};
-
-Pizza.prototype.completeXLM = function() {
-  return this.pizzaSize2 + " " + this.pizzaTopping2; 
-};
-
-Pizza.prototype.completeXL2 = function() {
-  return this.pizzaSize2 + " " + this.pizzaTopping1 + " " + this.pizzaTopping2; 
-};
-
-
-
-
-
-
-
-*/
+})
